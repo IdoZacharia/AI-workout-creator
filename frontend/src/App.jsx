bdfb7609001b96d30be23bcbf74d42cc13372590
IdoZacharia <idozacharia@gmail.com>
2026-03-22 17:19:23 +0200

    added basename to resolve routing error on GH Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './Home';
 import ResultsPage from './ResultsPage';
 import Footer from './Footer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename="/AI-workout-creator">
       <div className="flex flex-col min-h-screen bg-slate-950">
         <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/results" element={<ResultsPage />} />
+              <Route path="*" element={<HomePage />} />
             </Routes>
         </main>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
